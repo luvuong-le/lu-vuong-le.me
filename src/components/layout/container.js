9d28faf512dac1021b60d2c2604f7e2a7d48d68c
luvuong-le <lu-vuongle@hotmail.com>
2020-02-19 23:19:05 +1100

    formatted files

--- src/components/layout/container.js
+++ src/components/layout/container.js
@@ -1,12 +1,11 @@
 import React from "react"
-import { css } from "@emotion/core";
+import { css } from "@emotion/core"
 
 export default function Container({ styles, children }) {
-
     const containerStyles = css`
         padding: 0 2.5rem;
         margin: 4rem 0;
-    `;
+    `
 
     return <div css={containerStyles}>{children}</div>
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,25 +4,28 @@ import Layout from "@components/layout/layout"
 import Container from "@components/layout/container"
 import SEO from "@components/seo/seo"
 
-import LandingImage from "@assets/images/undraw-coding.svg";
-import Typed from "typed.js";
+import LandingImage from "@assets/images/undraw-coding.svg"
+import Typed from "typed.js"
 
 const IndexPage = () => {
-
-    const typedEl = React.createRef();
+    const typedEl = React.createRef()
 
     useEffect(() => {
         const typedJSOptions = {
-            strings: ['<i>> Software Engineer</i>', '<i>> Mentor</i>', '<i>> Designer</i>'],
+            strings: [
+                "<i>> Software Engineer</i>",
+                "<i>> Mentor</i>",
+                "<i>> Designer</i>",
+            ],
             typeSpeed: 40,
-            loop: true
-        };
+            loop: true,
+        }
 
-        let typed = new Typed(typedEl.current, typedJSOptions);
+        let typed = new Typed(typedEl.current, typedJSOptions)
 
         return () => {
-            typed.destroy();
-        };
+            typed.destroy()
+        }
     }, [typedEl])
 
     return (
@@ -35,18 +38,33 @@ const IndexPage = () => {
                 <Container>
                     <div className="landing">
                         <div className="landing__header">
-                            <h2>Welcome! <span class="anim anim--wave margin-l-sm" role="img" aria-label="jsx-a11y/accessible-emoji">👋</span></h2>
+                            <h2>
+                                Welcome!{" "}
+                                <span
+                                    class="anim anim--wave margin-l-sm"
+                                    role="img"
+                                    aria-label="jsx-a11y/accessible-emoji"
+                                >
+                                    👋
+                                </span>
+                            </h2>
                             <h1>I'm Lu-Vuong Le</h1>
                             <span class="typed fsize-l" ref={typedEl}></span>
                         </div>
                         <div className="landing__body">
                             <div className="flex">
                                 <p>I build web apps</p>
-                                <p>I create efficient solutions for companies</p>
+                                <p>
+                                    I create efficient solutions for companies
+                                </p>
                             </div>
                             <div className="landing__cta margin-t-md">
-                                <button className="btn btn--primary">Get In Touch</button>
-                                <button className="btn btn--primary">Resume</button>
+                                <button className="btn btn--primary">
+                                    Get In Touch
+                                </button>
+                                <button className="btn btn--primary">
+                                    Resume
+                                </button>
                             </div>
                             <div className="landing__img">
                                 <img src={LandingImage} alt="Undraw Coding" />
